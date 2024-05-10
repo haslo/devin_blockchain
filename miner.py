bf629bd7bb7b84ab849e7adeef52c4c7f8af8cc0
🤖 Devin <devin@example.com>
2024-05-10 16:32:45 +0000

    Implement mining functionality with miner.py

---
+++ miner.py
@@ -0,0 +1,51 @@
+import hashlib
+import json
+from time import time
+
+class Miner:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+        - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def mine(self, recipient_address):
+        """
+        This function is where you do the mining:
+        - Run the proof of work algorithm to get the next proof
+        - Reward for finding the proof is 1 HDC (haslo devin coin)
+        - Forge the new Block by adding it to the chain
+        """
+        last_block = self.blockchain.last_block
+        last_proof = last_block['proof']
+        proof = self.proof_of_work(last_proof)
+
+        # Reward for finding the proof
+        self.blockchain.new_transaction(
+            sender="0",  # indicates that this node has mined a new block
+            recipient=recipient_address,
+            amount=1,  # mining reward is 1 HDC
+        )
+
+        # Forge the new Block by adding it to the chain
+        previous_hash = self.blockchain.hash(last_block)
+        block = self.blockchain.new_block(proof, previous_hash)
+
+        return block
